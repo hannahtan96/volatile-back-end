9c859e70513d0b845e6ac22a63ad06760f1fe3ec
hannahtan96 <hannahtan96@gmail.com>
2023-01-25 22:31:27 -0800

    added .env back

--- app/routes/sentiment_routes.py
+++ app/routes/sentiment_routes.py
@@ -9,3 +9,7 @@ from time import sleep
 load_dotenv()
 
 sentiment_bp = Blueprint("sentiment_bp", __name__, url_prefix='/sentiment_api')
+
+SENTIMENT_KEY = os.environ.get("MEANING_CLOUD_API_KEY")
+SENTIMENT_URL = 'https://api.meaningcloud.com/sentiment-2.1'
+
